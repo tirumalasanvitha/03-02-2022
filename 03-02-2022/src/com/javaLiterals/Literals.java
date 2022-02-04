58ce4adc515ffa51e80bfaa518c66a3ba081f0fc
DELL <DELL@192.168.2.6>
2022-02-04 10:46:21 +0530

    Literals program added into the project 03-02-2022

---
+++ 03-02-2022/src/com/javaLiterals/Literals.java
@@ -0,0 +1,23 @@
+package com.javaLiterals;
+
+public class Literals 
+{
+    public static void main(String[] args) 
+    {
+    	/**
+    	 *write a java program to print Learner details(id,name and age)
+    	 */
+    	int Learnerid=20;
+    	String Learnername="sanvitha";
+    	int Learnerage=22;
+    	
+    	   System.out.print("Learnerid:");
+    	   System.out.println(Learnerid);
+    	   
+    	   System.out.print("Learnername:");
+    	   System.out.println(Learnername);
+    	   
+    	   System.out.print("Learnerage:");
+    	   System.out.println(Learnerage);
+    }
+}
